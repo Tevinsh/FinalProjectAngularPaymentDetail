3db58bff3e762e9a1a1ec2f9847fe4c423162b52
Tevinsh <pangeran.found@gmail.com>
2022-04-13 06:54:25 +0700

    add access control allow origin

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,31 +2,36 @@ import { Injectable } from '@angular/core';
 import { User } from './InterfaceUser';
 import { Observable,throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { signupUser } from './interface.signup.user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  httpOptions = {
+    headers: new HttpHeaders({ 
+      'Access-Control-Allow-Origin':'*'
+    })
+  };
   paymentDetailList : any;
   endpoint: string = 'https://tevinpaymentdetailsapi.herokuapp.com/';
   
   
   signIn(user: User): Observable<any> {
     let api = `${this.endpoint}api/AuthManagement/Login`;
-    return this.http.post(api,user).pipe( catchError(this.handleError))
+    return this.http.post(api,user,this.httpOptions).pipe( catchError(this.handleError))
   }
 
   signUp(user: signupUser): Observable<any> {
     let api = `${this.endpoint}api/AuthManagement/Register`;
-    return this.http.post(api,user).pipe( catchError(this.handleError))
+    return this.http.post(api,user,this.httpOptions).pipe( catchError(this.handleError))
   }
 
   
   getPD() : Observable<any> {
     let api = `${this.endpoint}api/PaymentDetail`;
-    return this.http.get(api).pipe( catchError(this.handleError)); 
+    return this.http.get(api,this.httpOptions).pipe( catchError(this.handleError)); 
   }
 
   getAuthorizationToken() {
